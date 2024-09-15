e3d9c87009d69e7413d10cd15d267e6e81a70a26
StasTolmachov <stas@tolmachov.dev>
2024-09-15 04:38:00 +0200

    2.1

--- main.go
+++ main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+type Car struct {
+	Brand string
+	Model string
+	Year  int
+}
+type Book struct {
+	Title         string
+	Author        string
+	Pages         int
+	PublishedYear int
+}
+
 func main() {
-	fmt.Println("help")
+	var car Car
+	car.Brand = "Tesla"
+	car.Model = "x"
+	car.Year = 2021
+	fmt.Println(car)
+
+	book := Book{Title: "taras", Author: "taras", Pages: 23, PublishedYear: 77}
+
+	fmt.Printf("Title %v Author %v Pages %v PublisherYear %v\n", book.Title, book.Author, book.Pages, book.PublishedYear)
 }
