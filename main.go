024c0296e00ea0600e9e82d5f10a91a70a6d3077
StasTolmachov <stas@tolmachov.dev>
2024-09-15 05:06:41 +0200

    3.1

--- main.go
+++ main.go
@@ -1,39 +1,15 @@
 package main
 
-import "fmt"
-
-type Car struct {
-	Brand string
-	Model string
-	Year  int
-}
-
-func (c Car) Greet() {
-	fmt.Printf("Car brand is %v model %v year %v\n", c.Brand, c.Model, c.Year)
-}
-
-func (c *Car) ChangeYear() {
-	c.Year++
-}
-
-type Book struct {
-	Title         string
-	Author        string
-	Pages         int
-	PublishedYear int
-}
-
-func (b Book) BookInfo() {
-	fmt.Printf("Title %v Author %v Pages %v PublisherYear %v\n", b.Title, b.Author, b.Pages, b.PublishedYear)
-}
+import (
+	"fmt"
+	"io/ioutil"
+)
 
 func main() {
-	car := Car{Brand: "Tesla", Model: "X", Year: 2024}
-	car.Greet()
-	car.ChangeYear()
-	car.Greet()
-
-	book := Book{Title: "taras", Author: "taras", Pages: 23, PublishedYear: 77}
-	book.BookInfo()
-
+	content, err := ioutil.ReadFile("data.txt")
+	if err != nil {
+		fmt.Println("error open file", err)
+		return
+	}
+	fmt.Printf("content from file: \n%v\n", string(content))
 }
